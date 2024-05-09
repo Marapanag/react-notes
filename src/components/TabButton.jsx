2e280f601a9a8b7cedeccc69b5851e0c8b6ed229
Maria Panagiotidou <marapanagiotidou98@gmail.com>
2024-05-09 20:55:16 +0300

    initial commit

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,8 @@
+export default function TabButton({ children, isSelected, ...props }) {
+
+    return (
+        <li>
+            <button className={isSelected ? 'active' : undefined} onClick={props.onClick}>{children}</button>
+        </li>
+    );
+}
